53a3a00e013136ddb886c7c9ea525f2c9ad75618
bigq <yum0e@protonmail.com>
2022-11-18 13:31:28 +0100

    feat: setup HydraS1AccounboundAttesterv2.sol contract and interface, logic not yet implemented

---
+++ contracts/attesters/hydra-s1/interfaces/IHydraS1AccountboundAttesterv2.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.14;
+pragma experimental ABIEncoderV2;
+
+import {IHydraS1SimpleAttester} from '././IHydraS1SimpleAttester.sol';
+
+/**
+ * @title Hydra-S1 Accountbound Interface
+ * @author Sismo
+ * @notice Interface with inherits from the errors, events and methods specific to the HydraS1SimpleAttester interface and Accountbound interface.
+ **/
+interface IHydraS1AccountboundAttesterv2 is IHydraS1SimpleAttester {
+
+}
