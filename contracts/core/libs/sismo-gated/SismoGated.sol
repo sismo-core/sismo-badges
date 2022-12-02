5b058ec98a1a3384bce3829a6350d9171f26b425
bigq <yum0e@protonmail.com>
2022-12-02 18:21:36 +0100

    feat: get nullifier from calling hydraS1Base with extradata

--- contracts/core/libs/sismo-gated/SismoGated.sol
+++ contracts/core/libs/sismo-gated/SismoGated.sol
@@ -6,13 +6,13 @@ import {Ownable} from '@openzeppelin/contracts/access/Ownable.sol';
 
 import {IBadges} from '../../interfaces/IBadges.sol';
 import {IAttestationsRegistry} from '../../interfaces/IAttestationsRegistry.sol';
-import {IAttester} from '../../interfaces/IAttester.sol';
+import {IHydraS1Base} from '../../../attesters/hydra-s1/base/IHydraS1Base.sol';
 
 import {Request, Claim, Attestation} from '../Structs.sol';
 
 contract SismoGated {
   IBadges public immutable BADGES;
-  IAttester public immutable ATTESTER;
+  IHydraS1Base public immutable ATTESTER;
 
   uint256 public immutable GATED_BADGE;
   mapping(uint256 => bool) private _isNullifierUsed;
@@ -23,15 +23,19 @@ contract SismoGated {
   /**
    * @dev Constructor
    * @param badgesAddress Badges contract address
+   * @param attesterAddress Hydra S1 attester contract address
+   * @param gatedBadge Badge token id that is required to call the function using `onlyBadgesOwner` modifier
    */
-  constructor(address badgesAddress, address attesterAddress, uint256 _gatedBadge) {
+  constructor(address badgesAddress, address attesterAddress, uint256 gatedBadge) {
     BADGES = IBadges(badgesAddress);
-    ATTESTER = IAttester(attesterAddress);
-    GATED_BADGE = _gatedBadge;
+    ATTESTER = IHydraS1Base(attesterAddress);
+    GATED_BADGE = gatedBadge;
   }
 
   /**
    * @dev Modifier allowing only the owners of the `GATED_BADGE` to trigger the function
+   * @param to Address of the badge owner
+   * @param data Data containing the user request and the proof associated to it
    * @notice a proof can also be sent to allow non-holders to prove their eligibility
    */
   modifier onlyBadgesOwner(address to, bytes calldata data) {
@@ -42,21 +46,24 @@ contract SismoGated {
       proveWithSismo(data);
     }
 
-    // to remove to hydra-s1
-    (uint256 nullifier, ) = abi.decode(
-      BADGES.getBadgeExtraData(to, GATED_BADGE),
-      (uint256, uint16)
-    );
+    bytes memory extraData = BADGES.getBadgeExtraData(to, GATED_BADGE);
+
+    uint256 nullifier = ATTESTER.getNullifierFromExtraData(extraData);
 
     if (_isNullifierUsed[nullifier]) {
       revert NFTAlreadyMinted();
     }
-    // mark the nullifier as used to prevent from bypassing this modifier several times with different destination address
+    // mark the nullifier as used to prevent from bypassing
+    // this modifier several times with different destination address
     _markNullifierAsUsed(nullifier);
 
     _;
   }
 
+  /**
+   * @dev Prove the user eligibility with Sismo
+   * @param data Bytes containing the user request and the proof associated to it
+   */
   function proveWithSismo(bytes memory data) public returns (uint256) {
     (Request memory request, bytes memory proofData) = _buildRequestAndProof(data);
 
@@ -67,6 +74,10 @@ contract SismoGated {
     return nullifier;
   }
 
+  /**
+   * @dev Build the request and the proof from data
+   * @param data Bytes containing the user request and the proof associated to it
+   */
   function _buildRequestAndProof(
     bytes memory data
   ) internal pure returns (Request memory, bytes memory) {
@@ -83,6 +94,10 @@ contract SismoGated {
     return (request, proofData);
   }
 
+  /**
+   * @dev Mark a nullifier as used
+   * @param nullifier Nullifier to mark as used
+   */
   function _markNullifierAsUsed(uint256 nullifier) internal {
     _isNullifierUsed[nullifier] = true;
   }
