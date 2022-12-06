dcc9ab2694f4a60519eb5d71467772c25d065f5d
bigq <yum0e@protonmail.com>
2022-12-06 15:58:17 +0100

    fix: change encoding from abi.encodePacked to abi.encode in HydraS1Accountbound

--- contracts/core/libs/sismo-gated/SismoGated.sol
+++ contracts/core/libs/sismo-gated/SismoGated.sol
@@ -2,61 +2,120 @@
 pragma solidity ^0.8.14;
 import 'hardhat/console.sol';
 
-import {SismoGatedState, Badges, HydraS1AccountboundAttester} from './SismoGatedState.sol';
+import {Badges} from '../../Badges.sol';
+import {Attester} from '../../Attester.sol';
+import {HydraS1AccountboundAttester} from '../../../attesters/hydra-s1/HydraS1AccountboundAttester.sol';
 import {HydraS1Base} from '../../../attesters/hydra-s1/base/HydraS1Base.sol';
 
 import {Request, Claim, Attestation} from '../Structs.sol';
 
-contract SismoGated is SismoGatedState {
+contract SismoGated {
+  // polygon contract address
+  address public constant BADGES_POLYGON_ADDRESS = 0xF12494e3545D49616D9dFb78E5907E9078618a34;
+  address public constant HYDRA_S1_ACCOUNTBOUND_POLYGON_ADDRESS =
+    0x10b27d9efa4A1B65412188b6f4F29e64Cf5e0146;
+
+  // goerli contract address
+  address public constant BADGES_GOERLI_ADDRESS = 0xE06B14D5835925e1642d7216F4563a1273509F10;
+  address public constant HYDRA_S1_ACCOUNTBOUND_GOERLI_ADDRESS =
+    0x89d80C9E65fd1aC8970B78A4F17E2e772030C1cB;
+
+  // mumbai contract address
+  address public constant BADGES_MUMBAI_ADDRESS = 0x5722fEa81027533721BA161964622271560da1aC;
+  address public constant HYDRA_S1_ACCOUNTBOUND_MUMBAI_ADDRESS =
+    0x069e6B99f4DA543156f66274FC6673442803C587;
+
+  // local contract address
+  address public constant BADGES_LOCAL_ADDRESS = 0xeF5b2Be9a6075a61bCA4384abc375485d5e196c3;
+  address public constant HYDRA_S1_ACCOUNTBOUND_LOCAL_ADDRESS =
+    0xf93A0C43A3466488D416628bf149495285e9f274;
+
+  /**************************************
+   * Storage slot
+   * 20 slots
+   * -> 3 slots used
+   * -> 1 used by badges, 1 used by hydraS1AccountboundAttester, 1 used by gatedBadge
+   * * 0x0 - 0x20: Badges contract
+   * 0x20 - 0x40: HydraS1AccountboundAttester contract
+   * 0x40 - 0x60: Gated badge token id
+   *
+   * -> 16 free slots
+   **************************************/
+
+  Badges public badges;
+  HydraS1AccountboundAttester public hydraS1AccountboundAttester;
+  uint256 public gatedBadge;
+
+  uint256[16] private _storagePlaceHolders;
+
   error UnsupportedNetwork();
   error UserIsNotOwnerOfBadge(uint256 collectionId);
-  error NFTAlreadyMinted();
 
   /**
    * @dev Constructor
-   * @param _gatedBadge Badge token id that is required to call the function using `onlyBadgesOwner` modifier
+   * @param gatedBadgeTokenId Badge token id that is required to call the function using `onlyBadgesOwner` modifier
    */
-  constructor(uint256 _gatedBadge) SismoGatedState(_gatedBadge) {}
+  constructor(uint256 gatedBadgeTokenId) {
+    // select the correct contract addresses based on the network
+    if (block.chainid == 137) {
+      badges = Badges(BADGES_POLYGON_ADDRESS);
+      hydraS1AccountboundAttester = HydraS1AccountboundAttester(
+        HYDRA_S1_ACCOUNTBOUND_POLYGON_ADDRESS
+      );
+    } else if (block.chainid == 5) {
+      badges = Badges(BADGES_GOERLI_ADDRESS);
+      hydraS1AccountboundAttester = HydraS1AccountboundAttester(
+        HYDRA_S1_ACCOUNTBOUND_GOERLI_ADDRESS
+      );
+    } else if (block.chainid == 80001) {
+      badges = Badges(BADGES_MUMBAI_ADDRESS);
+      hydraS1AccountboundAttester = HydraS1AccountboundAttester(
+        HYDRA_S1_ACCOUNTBOUND_MUMBAI_ADDRESS
+      );
+    } else if (block.chainid == 31337 || block.chainid == 1337) {
+      badges = Badges(BADGES_LOCAL_ADDRESS);
+      hydraS1AccountboundAttester = HydraS1AccountboundAttester(
+        HYDRA_S1_ACCOUNTBOUND_LOCAL_ADDRESS
+      );
+    } else {
+      revert UnsupportedNetwork();
+    }
+
+    gatedBadge = gatedBadgeTokenId;
+  }
 
   /**
    * @dev Modifier allowing only the owners of the `GATED_BADGE` to trigger the function
    * @param to Address of the badge owner
    * @param data Data containing the user request and the proof associated to it
-   * @param hydraS1Attester Attester contract used to verify the proof
+   * @param attesterAddress Attester contract used to verify the proof
    * @notice a proof can also be sent to allow non-holders to prove their eligibility
    */
   modifier onlyBadgesOwner(
     address to,
     bytes calldata data,
-    HydraS1Base hydraS1Attester
+    address attesterAddress
   ) {
     if (badges.balanceOf(to, gatedBadge) == 0) {
       if (data.length == 0) {
         revert UserIsNotOwnerOfBadge(gatedBadge);
       }
-      proveWithSismo(hydraS1Attester, data);
-    }
-
-    uint256 nullifier = _getNulliferForAddress(to);
-
-    if (isNullifierUsed(nullifier)) {
-      revert NFTAlreadyMinted();
+      proveWithSismo(Attester(attesterAddress), data);
     }
     _;
   }
 
   /**
    * @dev Prove the user eligibility with Sismo
-   * @param hydraS1Attester Attester contract used to verify the proof
+   * @param attester Attester contract used to verify proofs
    * @param data Bytes containing the user request and the proof associated to it
    */
   function proveWithSismo(
-    HydraS1Base hydraS1Attester,
+    Attester attester,
     bytes memory data
   ) public returns (Attestation memory) {
     (Request memory request, bytes memory proofData) = _buildRequestAndProof(data);
-
-    Attestation[] memory attestations = hydraS1Attester.generateAttestations(request, proofData);
+    Attestation[] memory attestations = attester.generateAttestations(request, proofData);
 
     return attestations[0];
   }
@@ -79,15 +138,5 @@ contract SismoGated is SismoGatedState {
     request.destination = newDestination;
 
     return (request, proofData);
-  }
-
-  /**
-   * @dev Getter to know a nullifier for a specific address
-   * @param to destination address referenced in the proof with this nullifier
-   */
-  function _getNulliferForAddress(address to) internal view returns (uint256) {
-    bytes memory extraData = badges.getBadgeExtraData(to, gatedBadge);
-    address badgeIssuerAddress = badges.getBadgeIssuer(to, gatedBadge);
-    return HydraS1Base(badgeIssuerAddress).getNullifierFromExtraData(extraData);
   }
 }
--- contracts/tests/MockGatedERC721.sol
+++ contracts/tests/MockGatedERC721.sol
@@ -1,19 +1,30 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.14;
+import 'hardhat/console.sol';
 
 import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
-import {SismoGated} from '../core/libs/sismo-gated/SismoGated.sol';
+import {SismoGated, HydraS1AccountboundAttester} from '../core/libs/sismo-gated/SismoGated.sol';
 
 contract MockGatedERC721 is ERC721, SismoGated {
+  mapping(uint256 => bool) private _isNullifierUsed;
+
+  error NFTAlreadyMinted();
+
   constructor(
-    uint256 _gatedBadge
-  ) ERC721('Sismo Gated NFT Contract', 'SGNFT') SismoGated(_gatedBadge) {}
+    uint256 gatedBadgeTokenId
+  ) ERC721('Sismo Gated NFT Contract', 'SGNFT') SismoGated(gatedBadgeTokenId) {}
 
   function safeMint(
     address to,
     uint256 tokenId,
     bytes calldata data
-  ) public onlyBadgesOwner(to, data, hydraS1AccountboundAttester) {
+  ) public onlyBadgesOwner(to, data, address(hydraS1AccountboundAttester)) {
+    uint256 nullifier = _getNulliferForAddress(to);
+
+    if (isNullifierUsed(nullifier)) {
+      revert NFTAlreadyMinted();
+    }
+
     _mint(to, tokenId);
   }
 
@@ -30,5 +41,31 @@ contract MockGatedERC721 is ERC721, SismoGated {
   function _afterTokenTransfer(address, address to, uint256, uint256) internal override(ERC721) {
     uint256 nullifier = _getNulliferForAddress(to);
     _markNullifierAsUsed(nullifier);
+  }
+
+  /**
+   * @dev Getter to know a nullifier for a specific address
+   * @param to destination address referenced in the proof with this nullifier
+   */
+  function _getNulliferForAddress(address to) internal view returns (uint256) {
+    bytes memory extraData = badges.getBadgeExtraData(to, gatedBadge);
+    address badgeIssuerAddress = badges.getBadgeIssuer(to, gatedBadge);
+    return HydraS1AccountboundAttester(badgeIssuerAddress).getNullifierFromExtraData(extraData);
+  }
+
+  /**
+   * @dev Getter to know if a nullifier has been used
+   * @param nullifier Nullifier to check
+   */
+  function isNullifierUsed(uint256 nullifier) public view returns (bool) {
+    return _isNullifierUsed[nullifier];
+  }
+
+  /**
+   * @dev Marks a nullifier as used
+   * @param nullifier Nullifier to mark as used
+   */
+  function _markNullifierAsUsed(uint256 nullifier) internal {
+    _isNullifierUsed[nullifier] = true;
   }
 }
