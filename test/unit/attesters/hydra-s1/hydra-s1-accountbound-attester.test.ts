70bfa2d0a0cd6e37707f216d71821863186ca1f4
bigq <yum0e@protonmail.com>
2022-12-06 14:54:35 +0100

    fix: indentation and proper test for accountbound

--- test/unit/attesters/hydra-s1/hydra-s1-accountbound-attester.test.ts
+++ test/unit/attesters/hydra-s1/hydra-s1-accountbound-attester.test.ts
@@ -609,27 +609,39 @@ describe('Test HydraS1 Accountbound Attester contract', () => {
           destinationSigner.address
         )
       ).to.be.true;
+    });
 
-      it("should get the nullifier and burn count from attestation's extra data", async () => {
-        const attestationExtraData = await attestationsRegistry.getAttestationExtraData(
-          group.properties.groupIndex,
-          destinationSigner.address
-        );
+    it("should get the nullifier and burn count from attestation's extra data", async () => {
+      const attestationExtraData = await attestationsRegistry.getAttestationExtraData(
+        (
+          await hydraS1AccountboundAttester.AUTHORIZED_COLLECTION_ID_FIRST()
+        ).add(group.properties.groupIndex),
+        destinationSigner.address
+      );
 
-        expect(attestationExtraData).to.be.eql(
-          encodeAccountBoundAttestationExtraData({
-            nullifier: inputs.publicInputs.nullifier,
-            burnCount: 0,
-          })
-        );
+      expect(attestationExtraData).to.be.eql(
+        encodeAccountBoundAttestationExtraData({
+          nullifier: inputs.publicInputs.nullifier,
+          burnCount: 0,
+        })
+      );
 
-        expect(
-          await hydraS1AccountboundAttester.getNullifierFromExtraData(attestationExtraData)
-        ).to.be.eql(inputs.publicInputs.nullifier);
-        expect(
-          await hydraS1AccountboundAttester.getBurnCountFromExtraData(attestationExtraData)
-        ).to.be.eql(0);
-      });
+      expect(
+        await hydraS1AccountboundAttester.getNullifierFromExtraData(
+          ethers.utils.defaultAbiCoder.encode(
+            ['uint256', 'uint16'],
+            [inputs.publicInputs.nullifier, 0]
+          )
+        )
+      ).to.be.eql(BigNumber.from(inputs.publicInputs.nullifier));
+      expect(
+        await hydraS1AccountboundAttester.getBurnCountFromExtraData(
+          ethers.utils.defaultAbiCoder.encode(
+            ['uint256', 'uint16'],
+            [inputs.publicInputs.nullifier, 0]
+          )
+        )
+      ).to.be.eql(0);
     });
 
     it('Should revert if the groupIndex has no cooldownDuration set', async () => {
