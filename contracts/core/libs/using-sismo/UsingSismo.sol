d2640124bda8fce521f6a63fe4e12b6a89726566
bigq <yum0e@protonmail.com>
2022-12-12 16:16:45 +0100

    feat: add some documentation and architecture

--- contracts/core/libs/using-sismo/UsingSismo.sol
+++ contracts/core/libs/using-sismo/UsingSismo.sol
@@ -13,6 +13,21 @@ enum BalanceRequirementType {
   equal
 }
 
+/**
+ * @title UsingSismo
+ * @notice This contract is intended to be used by other contracts that need to mint Sismo badges from Sismo proofs or check if an user holds certain Sismo Badges thanks
+ * to high level functions such as `_mintSismoBadge` and `_requireBadges`.
+ *
+ * This contract can be wiewed as a library that goes by pair with the "Prove With Sismo" off-chain flow. The "Prove With Sismo" off-chain flow is a flow that allows
+ * users to prove that they hold a specific Sismo badge and mint it on-chain thanks to a Sismo proof. The "Prove With Sismo" off-chain flow allows an user to:
+ *
+ * 1. Know if he is eligible to mint a Sismo badge
+ * 2. Generate a Sismo proof that proves that he can mint a Sismo Badge
+ * 3. Send the proof on-chain to mint the Sismo badge
+ *
+ * However, if a user wants to prove that he holds a specific Sismo badge without a proof, the `_requireBadges` function of this contract will check if he holds the badge or not.
+ */
+
 contract UsingSismo is Context {
   // this will be replaced by the contract registry
   HydraS1AccountboundAttester public immutable HYDRA_S1_ACCOUNTBOUND_ATTESTER =
@@ -32,14 +47,14 @@ contract UsingSismo is Context {
    * @dev Mint badges from sismoProof received by "Prove With Sismo" off-chain flow
    * @param request user request
    * @param sismoProof Sismo proof
-   * @param attester Attester address
+   * @param attester Attester address (default = HYDRA_S1_ACCOUNTBOUND_ATTESTER, see the next function)
    * @return Address of the owner of the badges, tokenIds of the badges minted and levels of the badges minted
-   * @notice This function will use the default attester (HYDRA_S1_ACCOUNTBOUND_ATTESTER) if no attester is specified (see the function below).
+   * @notice This function will use the default attester (HYDRA_S1_ACCOUNTBOUND_ATTESTER) if no attester is specified.
    */
   function _mintSismoBadge(
     Request memory request,
     bytes memory sismoProof,
-    address attester // default == HYDRA_S1_ACCOUNTBOUND_ATTESTER, see Section 3.
+    address attester // default == HYDRA_S1_ACCOUNTBOUND_ATTESTER, see next function.
   ) public returns (address, uint256[] memory, uint256[] memory) {
     (address owner, uint256[] memory badgesTokenIds, uint256[] memory badgesLevels) = Attester(
       attester
@@ -67,16 +82,24 @@ contract UsingSismo is Context {
   }
 
   /*******************************************************
-    2. CORE BADGE REQUIREMENTS FUNCTIONS, SEE BadgesRequirementsLib
+    2. CORE BADGE REQUIREMENTS FUNCTIONS
     (with default values, see section 4)
   *******************************************************/
 
+  /**
+   * @dev Check if the user has the required badges
+   * @param account User address
+   * @param badgeTokenIds TokenIds of the badges required
+   * @param isEachBadgeRequired If true, the user must have all the badges required. If false, the user must have at least one of the badges required. Default = false (see section 4).
+   * @param requiredBalances Required balances of the required badges. Default = [1, .., 1] (see section 4).
+   * @param balanceRequirementType Balance requirement type. Default = gte (see section 4).
+   */
   function _requireBadges(
     address account,
     uint256[] memory badgeTokenIds,
-    bool isEachBadgeRequired, // default = false, see Section 3.
-    uint256[] memory requiredBalances, // default = [1, .., 1], see Section 3.
-    BalanceRequirementType balanceRequirementType // default = [gte], see Section 3.
+    bool isEachBadgeRequired, // default = false, see Section 4.
+    uint256[] memory requiredBalances, // default = [1, .., 1], see Section 4.
+    BalanceRequirementType balanceRequirementType // default = [gte], see Section 4.
   ) internal view {
     _checkArgumentsLength(badgeTokenIds, requiredBalances);
 
@@ -116,11 +139,20 @@ contract UsingSismo is Context {
     3. MODIFIERS
   *******************************************************/
 
+  /**
+   * @dev Modifier to check if msg.sender has the required badge
+   * @param badgeTokenId TokenId of the badge required
+   */
   modifier onlyBadgeHolders(uint256 badgeTokenId) {
     _requireBadge(_msgSender(), badgeTokenId);
     _;
   }
 
+  /**
+   * @dev Modifier to check if msg.sender has the required badges
+   * @param badgeTokenIds TokenIds of the badges required
+   * @param isEachBadgeRequired If true, the user must have all the badges required. If false, the user must have at least one of the badges required. Default = false (see section 4).
+   */
   modifier onlyBadgesHolders(uint256[] memory badgeTokenIds, bool isEachBadgeRequired) {
     _requireBadges(_msgSender(), badgeTokenIds, isEachBadgeRequired);
     _;
@@ -149,21 +181,6 @@ contract UsingSismo is Context {
     _;
   }
 
-  modifier onlyBadgesHoldersWithExactBalance(
-    uint256[] memory badgeTokenIds,
-    uint256[] memory exactBalances,
-    bool isEachBadgeRequired
-  ) {
-    _requireBadges(
-      _msgSender(),
-      badgeTokenIds,
-      isEachBadgeRequired,
-      exactBalances,
-      BalanceRequirementType.equal
-    );
-    _;
-  }
-
   modifier onlyBadgesHoldersWithLowerBalance(
     uint256[] memory badgeTokenIds,
     uint256[] memory maxBalances,
@@ -179,29 +196,24 @@ contract UsingSismo is Context {
     _;
   }
 
+  modifier onlyBadgesHoldersWithExactBalance(
+    uint256[] memory badgeTokenIds,
+    uint256[] memory exactBalances,
+    bool isEachBadgeRequired
+  ) {
+    _requireBadges(
+      _msgSender(),
+      badgeTokenIds,
+      isEachBadgeRequired,
+      exactBalances,
+      BalanceRequirementType.equal
+    );
+    _;
+  }
+
   /*******************************************************
     4. CORE FUNCTIONS WITH DEFAULT VALUES HARD CODED (SAME NAME, DIFFERENT SIGNATURES)
   *******************************************************/
-
-  function _requireBadge(
-    address account,
-    uint256 badgeTokenId,
-    uint256 requiredBalance
-  ) internal view {
-    uint256[] memory badgeTokenIds = new uint256[](1);
-    badgeTokenIds[0] = badgeTokenId;
-
-    uint256[] memory requiredBalances = new uint256[](1);
-    requiredBalances[0] = requiredBalance;
-
-    _requireBadges(account, badgeTokenIds, false, requiredBalances, BalanceRequirementType.gte);
-  }
-
-  function _requireBadge(address account, uint256 badgeTokenId) internal view {
-    uint256[] memory badgeTokenIds = new uint256[](1);
-    badgeTokenIds[0] = badgeTokenId;
-    _requireBadges(account, badgeTokenIds);
-  }
 
   function _requireBadges(
     address account,
@@ -224,18 +236,35 @@ contract UsingSismo is Context {
     bool isEachBadgeRequired
   ) internal view {
     uint256[] memory arrayOfOnes = new uint256[](badgeTokenIds.length);
+    // by default, we require at least a balance of 1 for each badge
     for (uint32 i = 0; i < badgeTokenIds.length; i++) {
       arrayOfOnes[i] = 1;
     }
     _requireBadges(account, badgeTokenIds, isEachBadgeRequired, arrayOfOnes);
   }
 
+  function _requireBadge(
+    address account,
+    uint256 badgeTokenId,
+    uint256 requiredBalance
+  ) internal view {
+    uint256[] memory badgeTokenIds = new uint256[](1);
+    badgeTokenIds[0] = badgeTokenId;
+
+    uint256[] memory requiredBalances = new uint256[](1);
+    requiredBalances[0] = requiredBalance;
+
+    _requireBadges(account, badgeTokenIds, false, requiredBalances, BalanceRequirementType.gte);
+  }
+
   function _requireBadges(address account, uint256[] memory badgeTokenIds) internal view {
-    uint256[] memory arrayOfOnes = new uint256[](badgeTokenIds.length);
-    for (uint32 i = 0; i < badgeTokenIds.length; i++) {
-      arrayOfOnes[i] = 1;
-    }
     _requireBadges(account, badgeTokenIds, false);
+  }
+
+  function _requireBadge(address account, uint256 badgeTokenId) internal view {
+    uint256[] memory badgeTokenIds = new uint256[](1);
+    badgeTokenIds[0] = badgeTokenId;
+    _requireBadges(account, badgeTokenIds);
   }
 
   /**
@@ -252,6 +281,12 @@ contract UsingSismo is Context {
     }
   }
 
+  /**
+   * @dev Check if the balance of the badge is ok
+   * @param balance Balance of the badge
+   * @param requiredBalance Required balance of the badge
+   * @param balanceRequirementType Type of the balance requirement (gte, lte, equal)
+   */
   function _isBalanceRequirementOk(
     uint256 balance,
     uint256 requiredBalance,
