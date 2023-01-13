fd82cbaa0e70e2f4d780c380ab325d8d96ef6b32
Hadrien Charlanes <35774097+dhadrien@users.noreply.github.com>
2023-01-13 13:48:19 +0100

    feat: add version in sismo lib

--- contracts/core/SismoLib.sol
+++ contracts/core/SismoLib.sol
@@ -1,4 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.14;
+import {UsingSismo as _UsingSismo} from './libs/using-sismo/UsingSismo.sol';
 
-import './libs/using-sismo/UsingSismo.sol';
+contract SismoLibVersion {
+  uint256 public constant SISMO_LIB_VERSION = 1;
+}
+
+contract UsingSismo is _UsingSismo, SismoLibVersion {}
