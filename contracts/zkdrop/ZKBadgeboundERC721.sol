17e0fe87ecad6e8ce3986614964323a97e89a121
leo Sayous <leo@sismo.io>
2023-01-16 15:34:56 +0100

    refactor: use Request directly from Sismo Lib on the zkdrop contract

--- contracts/zkdrop/ZKBadgeboundERC721.sol
+++ contracts/zkdrop/ZKBadgeboundERC721.sol
@@ -2,8 +2,7 @@
 pragma solidity ^0.8.14;
 
 import '@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol';
-import {UsingSismo} from '../libs/SismoLib.sol';
-import {Request} from '../core/libs/Structs.sol';
+import {UsingSismo, Request} from '../libs/SismoLib.sol';
 import {AccessControl} from '@openzeppelin/contracts/access/AccessControl.sol';
 import {Pausable} from '@openzeppelin/contracts/security/Pausable.sol';
 import {Context} from '@openzeppelin/contracts/utils/Context.sol';
